2953fa5aef9e2968ae5fc1721065432f48326e61
Ikey Doherty <ikey@solus-project.com>
2017-08-26 17:48:49 +0100

    Stuff in a new Budgie::Panel::Window type
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/panel/window.cpp
@@ -0,0 +1,21 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include "window.h"
+#include <QDebug>
+
+namespace Budgie::Panel
+{
+    Window::Window()
+    {
+        qDebug() << "I r have a panel";
+    }
+}
---
+++ src/panel/window.h
@@ -0,0 +1,25 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Ikey Doherty <ikey@solus-project.com>
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QWidget>
+
+namespace Budgie::Panel
+{
+    class Window : public QWidget
+    {
+        Q_OBJECT
+
+    public:
+        explicit Window();
+    };
+}
