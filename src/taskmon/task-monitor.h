0332a2a9b6ff6d8aa5008ceda36e01c23d55f7a6
Ikey Doherty <ikey@solus-project.com>
2017-08-29 16:27:17 +0100

    Start skeleton TaskMon library
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/taskmon/task-monitor.h
@@ -0,0 +1,25 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QObject>
+
+namespace Task
+{
+    class Monitor : public QObject
+    {
+        Q_OBJECT
+
+    public:
+        explicit Monitor();
+    };
+}
