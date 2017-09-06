9df34be1ec01516ff13333d956aac4f45bfd13b1
Ikey Doherty <ikey@solus-project.com>
2017-09-06 16:15:46 +0100

    Add skeleton for the new budgie-rd-session binary
    This will be used for the overall session bootstrap, startup programs,
    as well as providing the shim environment to make GNOME applications work
    happily.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/session/manager.cpp
@@ -0,0 +1,20 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include "manager.h"
+
+namespace Session
+{
+    Manager::Manager(int &argc, char **argv) : QCoreApplication(argc, argv)
+    {
+        // TODO: Just about anything, really.
+    }
+}
---
+++ src/session/manager.h
@@ -0,0 +1,23 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include <QCoreApplication>
+
+namespace Session
+{
+    class Manager : public QCoreApplication
+    {
+        Q_OBJECT
+
+    public:
+        explicit Manager(int &argc, char **argv);
+    };
+}
