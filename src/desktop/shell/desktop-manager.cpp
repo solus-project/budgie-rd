34dd0f6bb830287fa20a5df4621369e14e8b2409
Ikey Doherty <ikey@solus-project.com>
2017-09-16 19:37:05 +0100

    Reformat + clang-format
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/desktop/shell/desktop-manager.cpp
+++ src/desktop/shell/desktop-manager.cpp
@@ -17,7 +17,8 @@
 namespace Desktop
 {
     Manager::Manager(QQmlEngine *engine)
-        : engine(engine), panelManager(new Panel::Manager(engine)), numScreens(0), primaryScreen(0), manageDesktop(false)
+        : engine(engine), panelManager(new Panel::Manager(engine)), numScreens(0), primaryScreen(0),
+          manageDesktop(false)
     {
         auto desktop = QApplication::desktop();
 
