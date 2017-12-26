b509b963e4c1b5cef65ed0095076110bf888a7e8
Ikey Doherty <ikey@solus-project.com>
2017-12-26 19:45:04 +0000

    shell: Add basic definition for PluginRegistry
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/shell/registry.cpp
@@ -0,0 +1,34 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include "registry.h"
+
+Budgie::PluginRegistry::PluginRegistry(){
+#warning "IMPLEMENT ME!"
+}
+
+Budgie::PluginRegistry::~PluginRegistry()
+{
+#warning "IMPLEMENT ME!"
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
---
+++ src/shell/registry.h
@@ -0,0 +1,41 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QObject>
+
+namespace Budgie
+{
+    /**
+     * The PluginRegistry is used to locate plugins by capability and load
+     * them to construct a dynamic desktop environment.
+     */
+    class PluginRegistry : public QObject
+    {
+        Q_OBJECT
+
+        explicit PluginRegistry();
+        ~PluginRegistry();
+    };
+}
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
