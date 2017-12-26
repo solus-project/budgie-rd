df120da3980a9f79485edff0516098ee80697f86
Ikey Doherty <ikey@solus-project.com>
2017-12-26 21:16:24 +0000

    Start implementing kwin interface (hacky)
    We need to move the service interface definition to a library but ofc
    we've been too lazy there so far.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/services/kwin/service.cpp
@@ -0,0 +1,35 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include "service.h"
+
+bool Budgie::KWinPlugin::start()
+{
+    return false;
+}
+
+bool Budgie::KWinPlugin::stop()
+{
+    return true;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
