3c5b3b1a4161305f6ce4a70a205d8f292c2fef28
Ikey Doherty <ikey@solus-project.com>
2017-12-28 16:14:08 +0000

    panel: Minor styling to help us flesh out demos
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/scaffolding/panel/window.cpp
+++ src/scaffolding/panel/window.cpp
@@ -9,17 +9,35 @@
  * version 2.1 of the License, or (at your option) any later version.
  */
 
+#include <QBoxLayout>
+
 #include "window.h"
 
 Budgie::PanelWindow::PanelWindow()
 {
     move(0, 1080 - 30);
     setFixedSize(1920, 30);
+    setAttribute(Qt::WA_TranslucentBackground);
     setAttribute(Qt::WA_X11NetWmWindowTypeDock);
     setAttribute(Qt::WA_X11DoNotAcceptFocus);
     setAttribute(Qt::WA_ShowWithoutActivating);
     setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
     setFocusPolicy(Qt::NoFocus);
+
+    auto rootWidget = new QWidget(this);
+    auto layout = new QHBoxLayout;
+    this->setLayout(layout);
+    layout->addWidget(rootWidget);
+    layout->setMargin(0);
+
+    // Clear any existing styles.
+    setStyleSheet("background-color: rgba(0, 0, 0, 0);");
+
+    // Colour picking
+    rootWidget->setObjectName("budgie-panel");
+    rootWidget->setStyleSheet("#budgie-panel { background-color: rgba(34, 49, 63, 0.89); }");
+    rootWidget->setLayout(new QBoxLayout(QBoxLayout::LeftToRight));
+    rootWidget->layout()->setMargin(0);
 }
 
 Budgie::PanelWindow::~PanelWindow()
