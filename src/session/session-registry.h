e06bd9c77609a27bff064236a4689cb7f10f90ca
Ikey Doherty <ikey@solus-project.com>
2017-12-28 22:01:12 +0000

    session: Add session specific PluginRegistry (SessionRegistry)
    This will be used to load our SessionModuleInterface plugins to provide
    extra support for the primary session, and ensure we have proper lifecycle
    maintainence. Also, its way easier to maintain stuff like this.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/session/session-registry.h
@@ -0,0 +1,61 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QSharedPointer>
+
+#include "registry.h"
+#include "session-module-interface.h"
+
+namespace Budgie
+{
+    /**
+     * Extend the core PluginRegistry to add shell specific functionality
+     */
+    class SessionRegistry : public PluginRegistry
+    {
+        Q_OBJECT
+
+    public:
+        explicit SessionRegistry();
+
+        /**
+         * Get a service from our known service providers by the given
+         * name.
+         *
+         * Implementations should ensure they check for nullptr here
+         */
+        QSharedPointer<SessionModuleInterface> getSessionModule(const QString &name);
+
+        /**
+         * Wrap hasPlugin to lookup the name with session/ prefix
+         */
+        bool hasSessionModule(const QString &name);
+
+        /**
+         * Unload a Session plugin
+         */
+        void unloadSessionModule(const QString &name);
+    };
+}
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
