5bdbbb2ff9f09079e7c1dbf92f5d89fe93c11d83
Ikey Doherty <ikey@solus-project.com>
2017-12-28 22:42:07 +0000

    session: Add skeletal ShellModule so we can continue implementing stuff
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/session/modules/shell/service.h
@@ -0,0 +1,47 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QObject>
+
+#include "session-module-interface.h"
+
+namespace Budgie
+{
+    /**
+     * Shell is launched after WindowManagerModule achieves a successful start
+     * and will attempt to launch the main shell process.
+     */
+    class Q_DECL_EXPORT ShellModule : public QObject, public SessionModuleInterface
+    {
+        Q_OBJECT
+        Q_PLUGIN_METADATA(IID "org.budgie-desktop.session.Shell" FILE "service.json")
+        Q_INTERFACES(Budgie::SessionModuleInterface)
+
+    public:
+        bool start() override;
+        bool stop() override;
+    };
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
