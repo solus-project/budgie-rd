3bb266c173d3112af398a864ab59873ee995be89
Ikey Doherty <ikey@solus-project.com>
2017-12-28 22:05:45 +0000

    session: Add new BudgieSession type (as yet unimplemented)
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/session/session.h
@@ -0,0 +1,49 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QObject>
+#include <QSharedPointer>
+
+#include "session-registry.h"
+
+namespace Budgie
+{
+    /**
+     * The Budgie::Session is the main entry into the Budgie Desktop, and
+     * uses modules to construct an appropriate session to support the
+     * primary shell and userland applications.
+     */
+    class Session : public QObject
+    {
+        Q_OBJECT
+
+    public:
+        explicit Session(const QString &name);
+
+    private:
+        QString m_sessionName;
+        QSharedPointer<SessionRegistry> m_registry;
+    };
+}
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
