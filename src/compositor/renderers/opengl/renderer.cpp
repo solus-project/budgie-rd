388204e47ba22703338288d73d4e1b2a5d40bbdc
Ikey Doherty <ikey@solus-project.com>
2018-01-02 00:16:07 +0000

    Add a nullop opengl renderer implementation
    We'll flesh this out to actually construct displays and views but for
    now it should be sufficient to allow refining the startup cycle.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/compositor/renderers/opengl/renderer.cpp
@@ -0,0 +1,39 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017-2018 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include <QDebug>
+
+#include "renderer.h"
+
+using namespace Budgie::Compositor;
+
+/**
+ * TODO: Wrap an OpenGLDisplay and return it
+ */
+Display *OpenGLRenderer::createDisplay(QWaylandOutput *output)
+{
+    Q_UNUSED(output);
+    qDebug() << "Oi. Implement me";
+    return nullptr;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
---
+++ src/compositor/renderers/opengl/renderer.h
@@ -0,0 +1,48 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017-2018 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QObject>
+#include <QWaylandOutput>
+
+#include "compositor-renderer-interface.h"
+#include "display.h"
+
+namespace Budgie::Compositor
+{
+    /**
+     * OpenGLRenderer provides an OpenGL based renderering plugin for the
+     * Budgie Compositor.
+     */
+    class Q_DECL_EXPORT OpenGLRenderer : public QObject, public RendererInterface
+    {
+        Q_OBJECT
+        Q_PLUGIN_METADATA(IID "org.budgie-desktop.compositor.OpenGLRenderer" FILE "renderer.json")
+        Q_INTERFACES(Budgie::Compositor::RendererInterface)
+
+    public:
+        Display *createDisplay(QWaylandOutput *output) override;
+    };
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
