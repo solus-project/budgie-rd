a6307ef2ca42d99cd04751d800996fe478241105
Ikey Doherty <ikey@solus-project.com>
2018-01-03 22:03:52 +0000

    Add a new basic WaylandWindow type
    We'll use this to associate a wl_shell/xdg_shell + root wayland surface
    together to provide logical units of representation.
    
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/compositor/wayland-window.cpp
@@ -0,0 +1,45 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017-2018 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include <QDebug>
+
+#include "wayland-window.h"
+
+using namespace Budgie::Compositor;
+
+/**
+ * Default constructor.
+ */
+WaylandWindow::WaylandWindow(SurfaceItem *rootSurface) : m_rootSurface(rootSurface)
+{
+}
+
+/**
+ * Return the surface considered to be our root.
+ * This surface may in turn have more ancestors.
+ */
+SurfaceItem *WaylandWindow::rootSurface()
+{
+    return m_rootSurface;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
---
+++ src/compositor/wayland-window.h
@@ -0,0 +1,61 @@
+/*
+ * This file is part of budgie-rd
+ *
+ * Copyright © 2017-2018 Budgie Desktop Developers
+ *
+ * budgie-rd is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <QObject>
+
+#include "compositor-common.h"
+#include "surface-item.h"
+
+namespace Budgie::Compositor
+{
+    /**
+     * Wrap up a Wayland surface as a toplevel window object within our view
+     * and input model. Effectively this maps a root surface (owned by the
+     * compositor) and tracks other metadata such as title, etc.
+     *
+     * Any SurfaceItem within the compositor is automatically mapped to a given
+     * output, the Window is used to provide a logical unit for presentation.
+     * Any non window surface will not be rendered unless it is a child surface
+     * of the windows root surface.
+     */
+    class WaylandWindow : public QObject
+    {
+        Q_OBJECT
+
+        friend class Server;
+
+    public:
+        SurfaceItem *rootSurface();
+
+    protected:
+        /**
+         * Construct a new SurfaceItem object for the given Wayland surface
+         */
+        explicit WaylandWindow(SurfaceItem *rootSurface);
+
+    private:
+        SurfaceItem *m_rootSurface;
+    };
+}
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=4 tabstop=4 expandtab:
+ * :indentSize=4:tabSize=4:noTabs=true:
+ */
